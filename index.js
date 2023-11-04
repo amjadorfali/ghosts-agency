2c6a62a1abea0c3562f30887be1269379b747875
Amjad Orfali <amjadorfali3@gmail.com>
2023-11-04 23:17:31 +0300

    🎉 Init project

---
+++ index.js
@@ -0,0 +1,12 @@
+// const button = document.getElementById("button");
+// const alertEl = document.getElementById("alert");
+
+// /**
+//  * @description onClick
+//  * @param {MouseEvent} _event
+//  */
+// const onClick = _event => {
+//     alertEl.hidden = !alertEl.hidden;
+// };
+
+// button.addEventListener("click", onClick);
